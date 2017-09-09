41c3aa4be3b3924687a80fb9f9e0dc8047da0e12
sonumax <sonumax2@gmail.com>
2017-09-09 18:22:57 +0300

    Create class witch help with work service

---
+++ src/main/java/ServiceHelper.java
@@ -0,0 +1,25 @@
+public class ServiceHelper {
+
+    private Object service;
+    private boolean lazy;
+    private Class<?> clazz;
+
+    public ServiceHelper(Object service, boolean lazy, Class<?> clazz) {
+        this.service = service;
+        this.lazy = lazy;
+        this.clazz = clazz;
+    }
+
+    public Class<?> getClazz() {
+        return clazz;
+    }
+
+    public Object getService() {
+        return service;
+    }
+
+    public boolean isLazy() {
+        return lazy;
+    }
+
+}
