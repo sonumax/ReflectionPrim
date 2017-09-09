36e0f0edc24a6f668e2baaf9feea05c0b792139e
sonumax <sonumax2@gmail.com>
2017-09-09 14:17:41 +0300

    Add new property to Service annotation

--- src/main/java/annotations/Service.java
+++ src/main/java/annotations/Service.java
@@ -1,5 +1,11 @@
 package annotations;
 
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Service {
 
     String name();
